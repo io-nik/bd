71062812a82a305bdc33bcb036015d12c3297def
Ivan Nikolaev <160820903+io-nik@users.noreply.github.com>
2026-05-23 00:23:36 +0300

    update lab8

---
+++ lab8-ACID/pahn_read_s1.sql
@@ -0,0 +1,15 @@
+BEGIN;
+SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+SHOW transaction_isolation;
+
+SELECT *
+FROM client
+WHERE enroll_date BETWEEN DATE '2023-08-08' AND DATE '2024-01-01';
+
+----- exetute phan_read_s2 fully
+
+SELECT *
+FROM client
+WHERE enroll_date BETWEEN DATE '2023-08-08' AND DATE '2024-01-01';
+
+COMMIT;
